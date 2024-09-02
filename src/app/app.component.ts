01d605b0e152585567b91833c1e7436eabb19a59
Shubham Dhanawade <shubhamdhanawade87@gmail.com>
2024-09-02 18:02:15 +0530

    First Coopy but add pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./Component/navbar/navbar.component";
+import { FooterComponent } from "./Component/footer/footer.component";
+import { HomeComponent } from "./Pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
