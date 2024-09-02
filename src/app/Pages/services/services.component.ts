0fa672bcce97cac8f5bb7203e5a862d13761c938
KrushnaKharat <krushnakharat.official@gmail.com>
2024-09-02 18:46:32 +0530

    Add Object In Ts

--- src/app/Pages/services/services.component.ts
+++ src/app/Pages/services/services.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [FormsModule , NgFor],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+
+  myData:any = [
+    {
+      service:"Service 1",
+      details:"abcd",
+      price:23
+    },
+    {
+      service:"Service 2",
+      details:"abcd",
+      price:23
+    },{
+      service:"Service 3",
+      details:"abcd",
+      price:23
+    }
+  ]
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Component/navbar/navbar.component";
 import { FooterComponent } from "./Component/footer/footer.component";
 import { HomeComponent } from "./Pages/home/home.component";
+import { FormsModule } from '@angular/forms';
+import { ServicesComponent } from "./Pages/services/services.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, FormsModule, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
