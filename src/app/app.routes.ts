7db8a819d34f8085465123e842a2c104fbb4498f
KrushnaKharat <krushnakharat.official@gmail.com>
2024-09-03 18:19:07 +0530

    Creating Router Links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ServicesComponent } from './Pages/services/services.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactUsComponent } from './Pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'service',
+        component:ServicesComponent
+    },
+    {
+        path:'about',
+        component:AboutUsComponent
+    },
+    {
+        path:'contact',
+        component:ContactUsComponent
+    }
+];
